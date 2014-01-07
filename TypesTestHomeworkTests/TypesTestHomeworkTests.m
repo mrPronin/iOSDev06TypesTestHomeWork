d009fa69f56d1f01e1d5fa63c3c298c2065c28ad
Alexander Pronin <pronin.alx@gmail.com>
2014-01-07 17:40:04 +0200

    First commit

---
+++ TypesTestHomeworkTests/TypesTestHomeworkTests.m
@@ -0,0 +1,34 @@
+//
+//  TypesTestHomeworkTests.m
+//  TypesTestHomeworkTests
+//
+//  Created by Pronin Alexander on 07.01.14.
+//  Copyright (c) 2014 Pronin Alexander. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface TypesTestHomeworkTests : XCTestCase
+
+@end
+
+@implementation TypesTestHomeworkTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
