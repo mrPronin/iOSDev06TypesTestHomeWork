736407cfcff5102dcf12c8d7f7a744c52ccb30ba
Alexander Pronin <pronin.alx@gmail.com>
2014-01-20 01:02:59 +0200

    Third commit

---
+++ TypesTestHomework/RITEnumTest.h
@@ -0,0 +1,13 @@
+//
+//  RITEnumTest.h
+//  TypesTestHomework
+//
+//  Created by Pronin Alexander on 07.01.14.
+//  Copyright (c) 2014 Pronin Alexander. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface RITEnumTest : NSObject
+
+@end
---
+++ TypesTestHomework/RITEnumTest.m
@@ -0,0 +1,13 @@
+//
+//  RITEnumTest.m
+//  TypesTestHomework
+//
+//  Created by Pronin Alexander on 07.01.14.
+//  Copyright (c) 2014 Pronin Alexander. All rights reserved.
+//
+
+#import "RITEnumTest.h"
+
+@implementation RITEnumTest
+
+@end
